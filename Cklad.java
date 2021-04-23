940f821c80b8dbd889cc335ca6cfa64cdb0460d2
ProstoKryg <82756892+ProstoKryg@users.noreply.github.com>
2021-04-23 12:04:00 +0900

    ver 1

---
+++ Cklad.java
@@ -0,0 +1,85 @@
+package cklad;
+
+public class Cklad {
+
+    int Prod [] = new int [] {0,0,0,0,0,0};
+    
+    int posl [] = new int [] {0,0,0,0,0,0,0,0,0,0};
+    
+    int ProdPos1 [] = new int [] {0,0,0,0,0,0};
+    
+    int ProdPol1 [] = new int [] {0,0,0,0,0,0};
+    
+    
+    
+    boolean stopped=false;
+
+Thread Pos1=new Thread()
+	{	public void run(){
+		 while(!stopped)
+			{
+                            try	
+				{sleep((int)(Math.random()*10000));
+                                
+                                    
+                                    
+                                
+                                
+					System.out.println();
+				}
+		  		catch(Exception pe)
+		  		{	
+					System.out.println("Error heigh: " + pe);
+		  		}
+		  	}
+		  }
+	};
+
+Thread Pol1=new Thread()
+	{	public void run()
+		{ while(!stopped)
+			{try	
+				{ sleep(1);
+
+				System.out.println();
+				}
+		  	catch(Exception pe)
+		  		{	
+					System.out.println("Error heigh: " + pe);
+		  		}
+		  	}
+		  }
+	};
+
+Thread Oper=new Thread()
+	{	public void run()
+		{ while(!stopped)
+			{try	
+				{ sleep(1);
+
+				System.out.println();
+				}
+		  	catch(Exception pe)
+		  		{	
+					System.out.println("Error heigh: " + pe);
+		  		}
+		  	}
+		  }
+	};
+
+	public Cklad()
+	{	
+		Pol1.setPriority(Thread.NORM_PRIORITY);	//NORM_PRIORITY MIN_PRIORITY 
+                Pos1.setPriority(Thread.NORM_PRIORITY);
+                Oper.setPriority(Thread.NORM_PRIORITY);
+    
+		Pol1.start();	
+		Pos1.start();	
+		Oper.start();	
+	}
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
